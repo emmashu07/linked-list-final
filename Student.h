115b4232bcb8365dce8b087555677561189a0e3c
emmashu07 <emmashu@DESKTOP-TTUPTH2.localdomain>
2020-01-19 23:34:11 -0800

    Add function

---
+++ Student.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+//Emma Shu, 12/13/2019: The header file to the student class that is stored in the node class.
+
+class Student {
+	public:
+		Student(char* firstName, char* secondName, int id, float gpa);
+		virtual ~Student();
+		void print();
+		int getId();
+		float getGpa();
+	private:
+		char* firstName;
+		char* secondName;
+		int id;
+		float gpa;
+};		
